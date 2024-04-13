f974041f7d1bd50168ca53365ce81f18e4790494
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-04-13 18:06:04 +0530

    Update q7.c

--- iNeuron assignment 14/q7.c
+++ iNeuron assignment 14/q7.c
@@ -26,3 +26,39 @@ int main()
 Enter the numbers: 987 234 1 678 1000
 Second largest element in array is 987
 */
+//-------------------------------------OR---------------------------------
+/*
+#include<stdio.h>
+int main()
+{
+    int n,a[10],largest=0,s_largest=0;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+
+    printf("Enter array elements: ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    largest=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>largest)
+        {
+            s_largest=largest;
+            largest=a[i];
+        }
+        else
+        {
+            if(a[i]>s_largest)
+                s_largest=a[i];
+        }
+    }
+    printf("Second largest element is %d",s_largest);
+}
+OUTPUT-
+Enter the size of array: 4
+Enter array elements: 6 2 1 9
+Second largest element is 6
+*/
