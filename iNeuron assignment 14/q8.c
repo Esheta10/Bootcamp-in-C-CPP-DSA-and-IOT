221dfd710dcd2b6347d93322fd9cf36bd80cc3e7
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-04-15 19:48:02 +0530

    Update q8.c

--- iNeuron assignment 14/q8.c
+++ iNeuron assignment 14/q8.c
@@ -26,3 +26,29 @@ int main()
 Enter the numbers: 9 1 3 6 2
 second smallest element in array is 2
 */
+//---------------------------------OR------------------------------------------
+#include<stdio.h>
+int main()
+{
+    int a[5],i,j,smallest=0,s_smallest=0;
+    printf("Enter the numbers: ");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    smallest=a[0];
+    for(i=0;i<5;i++)
+    {
+        if(a[i]<smallest)
+        {
+            s_smallest=smallest;
+            smallest=a[i];
+        }
+        else
+        {
+            if(a[i]<s_smallest)
+                s_smallest=a[i];
+        }
+    }
+    printf("Second smallest element is %d",s_smallest);
+}
