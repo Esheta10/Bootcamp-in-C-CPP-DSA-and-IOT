efc9064a778b31ecbea3b9efb8bb3331f0eb9248
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-04-28 21:15:43 +0530

    Update q10.c

--- iNeuron Assignment 18/q10.c
+++ iNeuron Assignment 18/q10.c
@@ -4,26 +4,30 @@
 int repeated_character(char a[]);
 int main()
 {
-    char a[]="abcddabcefghabc";
+    char a[]="abcddabcefghabchhhh";
     repeated_character(a);
 }
 int repeated_character(char a[])
 {
-    int freq[100]={0};
+    int freq[150]={0}; //since all ASCII characters get covered under 150 range
     int i=0;
     while(a[i] != '\0')
     {
-        freq[a[i]]++;
+        freq[a[i]]++;//a[i] index mein rakhi value ko increment karna hai frequency array mein
         i++;
     }
-    for(i=0;i<100;i++)
+    for(i=0;i<150;i++)//frequency array
     {
-        if(freq[i]>1)
-            printf("%c --> %d\n",i,freq[i]);
+        if(freq[i]>1)//sirf unhi frequencies ko print karna hai jo repeated hai
+            printf("%c --> %d\n",i,freq[i]);//to print only the characters that are repeated.
+            //i is the ASCII value of the character being printed.
+            //freq[i] is the frequency of the character stored in the freq array at index i
     }
 }
 /*OUTPUT-
 a --> 3
 b --> 3
 c --> 3
+d --> 2
+h --> 5
 */
