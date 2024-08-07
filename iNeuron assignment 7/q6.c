d46b23462228ef57ff51e5bc8b15aa0fb4cfc2ef
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-08-07 15:42:53 +0530

    Update q6.c

--- iNeuron assignment 7/q6.c
+++ iNeuron assignment 7/q6.c
@@ -2,21 +2,25 @@
 //Prime number-->any no. divisible by 1 and itself.eg-2,3,5,7,...
 //to find prime numbers in range, let min=1 and max=given range
 #include<stdio.h>
+#include<stdio.h>
 int main()
 {
-    int min,max,i,j,count;
-    printf("Enter the value of min and max- ");
-    scanf("%d %d",&min,&max);
-    for(i=min;i<=max;i++)
+    int n;
+    for(int n=1;n<=100;n++)
     {
-        count=0;
-        for(j=1;j<=i;j++)
+        int flag=0;
+        for(int i=2;i<=n/2;i++)
         {
-            if(i%j==0)
-                count++;
+            if(n%i==0)
+            {
+                flag=1;
+                break;
+            }
         }
-        if(count==2)
-            printf("%d ",i);
+         if(flag==0)
+            printf("%d ",n);
     }
     return 0;
 }
+/*output-
+1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97  */
